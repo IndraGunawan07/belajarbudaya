538edca09543035868f9ccff1db58588e5e5e104
alvin2723 <alvinjulian87@gmail.com>
2020-11-27 16:13:24 +0700

    update add question

--- src/app/add-question/add-question.page.ts
+++ src/app/add-question/add-question.page.ts
@@ -16,8 +16,18 @@ export class AddQuestionPage implements OnInit {
 
   ngOnInit() {
     this.fireStore.collection('SoalQuiz').add({
-      answer1: 'Bosku',
       question: 'Apa itu Bosku?',
+      answer1: 'Bosku',
+      answer2: '',
+      answer3: '',
+      answer4: '',
+      category: '',
+      description: '',
+      imgUrl: '',
+      level: '',
+      region: '',
+      title: '',
+      trueAnswer: '',
       // Other info you want to add here
     });
   }
